b4ff486e9e70f2e979a67af250d9c22590b304c1
Nitin345614 <nitinsingha345614@gmail.com>
2024-11-23 10:35:42 +0100

    added 2 more threads. Only 2 more remain

--- interprocess/router_dealer.c
+++ interprocess/router_dealer.c
@@ -34,12 +34,15 @@ char dealer2worker1_name[30];
 char dealer2worker2_name[30];
 char worker2dealer_name[30];
 
+//Creates mutex for access to buffer
+pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+
 //Creates request holding variables
 //Request nextRequest;
 
 //Creates circular buffers for data transfers
-  req_queue_x buffer_c2d[10]; int in_c2d = 0; int out_c2d = 0; int count = 0; int size_req = sizeof(req_queue_x);
-  S1_queue_X buffer_d2w[10];
+  req_queue_x buffer_c2d[10]; int in_c2d = 0; int out_c2d = 0; int count1 = 0; int size_req = sizeof(req_queue_x);
+  S1_queue_X buffer_d2w[10]; int in_d2w = 0; int out_d2w = 0; int count2 = 0; int size_rsp = sizeof(S1_queue_X);
   Rsp_queue_X buffer_w2d[10];
 
 //Threading functions
@@ -50,16 +53,61 @@ void c2d_thread(struct mq_attr * attr){
   while(true){
 	  //Blocking read of client queue, data is stored in c2d buffer
 	  //If statement is used to make sure that the buffer is not overloaded
-	  if(count<10) { //critical section of thread
-		  pthread_mutex_lock(&m_c2d);
-		  mq_receive (mg_c2d, buffer_c2d[in_c2d], size_req, NULL);
+	  if(count1<10) { //critical section of thread
+		  pthread_mutex_lock(&m);
+		  mq_receive(mg_c2d, buffer_c2d[in_c2d], size_req, NULL);
 	  //Increments array variables
 		  in_c2d = (in_c2d+1)%10;
-	      ++count;
-	      pthread_mutex_unlock(&m_c2d);
+	      ++count1;
+	      pthread_mutex_unlock(&m);
 	      } 
 	  else sleep(0.00001);
 	  //If the buffer is overloaded, the thread sleeps for 10 microseconds to allow the buffer to unload
+  }
+}
+
+void forwarding_thread(){
+	//This thread processes client data into the format needed by workers
+	S1_queue_X rsp;
+  while(true){
+	  if(count1>0) { //critical section of thread
+		  pthread_mutex_lock(&m);
+		  //Extract data from buffer_c2d then put it in d2w and update buffer variables
+		  rsp.request_id = buffer_c2d[out].request_id;
+		  rsp.data = buffer_c2d[out].data;
+		  
+		  out_c2d = (out_c2d+1)%10;
+	      --count1;
+	      
+	      buffer_d2w[in] = rsp;
+	      
+	      in_d2w = (in_d2w+1)%10;
+	      ++count2;
+	      
+	      pthread_mutex_unlock(&m);
+	      } 
+	  else sleep(0.00001);
+	  //If the buffer is empty, the thread sleeps for 10 microseconds to allow the buffer to fill
+  }
+}
+
+void d2w_thread(struct mq_attr * attr){
+  //Creates queue according to fed parameters to send worker data
+  mqd_t mq_d2w = mq_open (dealer2worker1_name, O_RONLY | O_CREAT | O_EXCL, 0666, attr);
+  
+  while(true){
+	  //Transfer data from client buffer to worker queue
+	  //If statement is used to make sure that the buffer has data to transfer
+	  if(count2>0) { //critical section of thread
+		  pthread_mutex_lock(&m);
+		  mq_send(mg_d2w, buffer_d2w[out_d2w], size_rsp, NULL);
+	  //Increments array variables
+		  out_d2w = (out_d2w+1)%10;
+	      --count2;
+	      pthread_mutex_unlock(&m);
+	      } 
+	  else sleep(0.00001);
+	  //If the buffer is empty, the thread sleeps for 10 microseconds to allow the buffer to fill
   }
 }
 
@@ -74,24 +122,28 @@ int main (int argc, char * argv[])
   struct mq_attr attr_c2d; //client to dealer ... and so on. c = client, d = dealer, w = worker
   struct mq_attr attr_d2w;
   struct mq_attr attr_w2d;
-  struct mq_attr attr_d2c; 
 
   attr_c2d.mq_maxmsg  = 10;
   attr_c2d.mq_msgsize = sizeof (req_queue_x);
+  
+  attr_d2w.mq_maxmsg  = 10;
+  attr_d2w.mq_msgsize = sizeof (S1_queue_X);
+  
+  attr_w2d.mq_maxmsg  = 10;
+  attr_w2d.mq_msgsize = sizeof (Rsp_queue_X);
 
-
-  //Creates mutexes for each buffer
-  pthread_mutex_t m_c2d = PTHREAD_MUTEX_INITIALIZER;
-  pthread_mutex_t m_c2w = PTHREAD_MUTEX_INITIALIZER;
-  pthread_mutex_t m_w2d = PTHREAD_MUTEX_INITIALIZER;
 
 
   //Creates 4 threads, one for each data flow path (c2d, d2w, w2d, d2c) so that all 4 queues can excecute simultaneously
   //Each threading function uses mutexes to prevent race conditions
-  pthread_t c2dthread; //"forward" thread client2worker
-  pthread_t bthread; //"backward" thread worker2 client
-  pthread_create(&c2dthread, NULL, c2d_thread(), attr_c2d)
+  pthread_t c2dthread; pthread_t fthread;   pthread_t d2wthread;   pthread_t w2dthread;   pthread_t d2cthread; 
+  pthread_create(&c2dthread, NULL, c2d_thread(), attr_c2d);
+  pthread_create(  &fthread, NULL, forwarding_thread(), NULL);
+  pthread_create(&d2wthread, NULL, d2w_thread(), attr_d2w)
   
+  pthread_create(&w2dthread, NULL, w2d_thread(), attr_w2d)
+  pthread_create(&d2cthread, NULL, d2c_thread(), attr_d2c)
+
   
   
   // TODO:
