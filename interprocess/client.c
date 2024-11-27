0eefd9c131cf01cd26b7b296a44f57d4a9e79122
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2024-11-27 12:46:56 +0100

    naming file update

--- interprocess/client.c
+++ interprocess/client.c
@@ -2,7 +2,7 @@
  * Operating Systems  (2INCO)  Practical Assignment
  * Interprocess Communication
  *
- * STUDENT_NAME_1 (STUDENT_NR_1)
+ * Daniel Tyukov (1819283)
  * STUDENT_NAME_2 (STUDENT_NR_2)
  *
  * Grading:
