dd5f1f5909ce461833b02aad586d6fc1bc8b4ee1
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2024-11-29 06:54:06 +0100

    naming update

--- interprocess/router_dealer.c
+++ interprocess/router_dealer.c
@@ -78,15 +78,15 @@ int main (int argc, char * argv[])
     attr_w2d.mq_msgsize = size_res;
     attr_w2d.mq_curmsgs = 0;
 
-    mq_unlink(client2dealer_name);
-    mq_unlink(dealer2worker1_name);
-    mq_unlink(dealer2worker2_name);
-    mq_unlink(worker2dealer_name);
+    mq_unlink(client2dealer_21);
+    mq_unlink(dealer2worker1_21);
+    mq_unlink(dealer2worker2_21);
+    mq_unlink(worker2dealer_21);
 
-    mqd_t mq_c2d = mq_open (client2dealer_name, O_RDONLY | O_CREAT | O_NONBLOCK, 0600, &attr_c2d);
-    mqd_t mq_d2w = mq_open (dealer2worker1_name, O_WRONLY | O_CREAT | O_NONBLOCK, 0600, &attr_d2w);
-    mqd_t mq_d2w2 = mq_open (dealer2worker2_name, O_WRONLY | O_CREAT | O_NONBLOCK, 0600, &attr_d2w2);
-    mqd_t mq_w2d = mq_open (worker2dealer_name, O_RDONLY | O_CREAT | O_NONBLOCK, 0600, &attr_w2d);
+    mqd_t mq_c2d = mq_open (client2dealer_21, O_RDONLY | O_CREAT | O_NONBLOCK, 0600, &attr_c2d);
+    mqd_t mq_d2w = mq_open (dealer2worker1_21, O_WRONLY | O_CREAT | O_NONBLOCK, 0600, &attr_d2w);
+    mqd_t mq_d2w2 = mq_open (dealer2worker2_21, O_WRONLY | O_CREAT | O_NONBLOCK, 0600, &attr_d2w2);
+    mqd_t mq_w2d = mq_open (worker2dealer_21, O_RDONLY | O_CREAT | O_NONBLOCK, 0600, &attr_w2d);
 
     // Check if queues were opened correctly
     if ((mq_c2d == (mqd_t) -1) || (mq_d2w == (mqd_t) -1) || (mq_d2w2 == (mqd_t) -1) || (mq_w2d == (mqd_t) -1))
@@ -108,7 +108,7 @@ int main (int argc, char * argv[])
     }
     else if (clientID == 0)
     {
-        execlp ("./client", "client", client2dealer_name, NULL);
+        execlp ("./client", "client", client2dealer_21, NULL);
         perror ("Client execlp() failed");
         exit(EXIT_FAILURE);
     }
@@ -125,7 +125,7 @@ int main (int argc, char * argv[])
         else if (workerID == 0)
         {
             // In child process
-            execlp ("./worker_s1", "worker_s1", dealer2worker1_name, worker2dealer_name, NULL);
+            execlp ("./worker_s1", "worker_s1", dealer2worker1_21, worker2dealer_21, NULL);
             perror ("Worker_s1 execlp() failed");
             exit(EXIT_FAILURE);
         }
@@ -144,7 +144,7 @@ int main (int argc, char * argv[])
         else if (worker2ID == 0)
         {
             // In child process
-            execlp ("./worker_s2", "worker_s2", dealer2worker2_name, worker2dealer_name, NULL);
+            execlp ("./worker_s2", "worker_s2", dealer2worker2_21, worker2dealer_21, NULL);
             perror ("Worker_s2 execlp() failed");
             exit(EXIT_FAILURE);
         }
@@ -261,10 +261,10 @@ int main (int argc, char * argv[])
     mq_close(mq_w2d);
 
     // Unlink the message queues
-    mq_unlink(client2dealer_name);
-    mq_unlink(dealer2worker1_name);
-    mq_unlink(dealer2worker2_name);
-    mq_unlink(worker2dealer_name);
+    mq_unlink(client2dealer_21);
+    mq_unlink(dealer2worker1_21);
+    mq_unlink(dealer2worker2_21);
+    mq_unlink(worker2dealer_21);
 
     return 0;
 }
